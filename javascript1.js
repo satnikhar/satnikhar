0787b891a06f433084965c665c57e9770f89fef6
satnikhar <sat_nikhar@hotmail.com>
2024-10-02 14:35:16 +0530

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,12 @@
+
+let dvs = document.querySelector("#dvs");
+
+dvs.onmouseover = ()=>{
+console.log('you are inside div');
+
+};
+let btns = document.querySelector("#btns");
+
+btns.onclick=()=>{
+    console.log('button clicked' );
+};
